cc56b5c83898d6828694aa5baf84be7518db2516
Kanta Tamura <kanta03085@gmail.com>
2021-01-16 22:23:28 +0900

    add: Parser struct

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,61 @@ impl Source {
             pos: 0,
         }
     }
+
+    fn next(&mut self) {
+        self.pos += 1;
+    }
 }
 
 fn main() {
-    let mut form = Source::new("124567+12");
-    println!("{:?}", number(&mut form));
+    let mut form = Parser::new("1987+1234");
+    println!("{:?}", form.number());
 
     println!("{:?}", form);
 }
 
-// number := 1|2|3|4|5|6|7|8|9|0 +
-fn number(s: &mut Source) -> String {
-    let start = s.pos;
-    for (iter, num) in s.form.chars().enumerate() {
-        if iter < start { continue; }
-        if !num.is_numeric() {
-            s.pos = iter;
-            return s.form
-                .chars()
-                .enumerate()
-                .filter(|&(i, _)| i >= start && i < iter)
-                .fold("".to_string(), |s, (_, c)| format!("{}{}", s, c));
+#[derive(Debug)]
+struct Parser {
+    source: Source,
+}
+
+impl Parser {
+    pub fn new(form: &str) -> Self {
+        Self {
+            source: Source::new(form),
         }
     }
-    s.form
-        .chars()
-        .enumerate()
-        .fold("".to_string(), |s, (_, c)| format!("{}{}", s, c))
+
+    pub fn next(&mut self) {
+        self.source.next();
+    }
+
+    pub fn number(&mut self) -> i32 {
+        self.number_str().parse::<i32>().unwrap()
+    }
+
+    // number := 1|2|3|4|5|6|7|8|9|0 +
+    fn number_str(&mut self) -> String {
+        // 読み込み開始位置
+        let read_start = self.source.pos;
+        let form_enu = self.source.form.chars().enumerate();
+
+        for (iter, fig) in self.source.form.chars().enumerate() {
+            // 読み込み開始位置まで読み飛ばし
+            if iter < read_start { continue; }
+            self.source.pos = iter;
+            // 数字でなければ、そこまでの数字を返す
+            if !fig.is_numeric() {
+                return form_enu
+                    .filter(|&(i, _)| i >= read_start && i < iter)
+                    .fold("".to_string(), |s, (_, c)| format!("{}{}", s, c));
+            }
+        }
+
+        form_enu
+            .filter(|&(i, _)| i >= read_start)
+            .fold("".to_string(), |s, (_, c)| format!("{}{}", s, c))
+    }
 }
 
 #[cfg(test)]
@@ -46,22 +74,22 @@ mod tests {
 
     #[test]
     fn number_test() {
-        let mut form_0 = Source::new("");
-        let mut form_1 = Source::new("1");
-        let mut form_2 = Source::new("25");
-        let mut form_3 = Source::new("1+1");
-        let mut form_4 = Source::new("12+21+43");
+        let mut form_0 = Parser::new("");
+        let mut form_1 = Parser::new("1");
+        let mut form_2 = Parser::new("25");
+        let mut form_3 = Parser::new("1+1");
+        let mut form_4 = Parser::new("12+21+43");
 
         // 先頭の連続する数字を返す
-        assert_eq!(number(&mut form_0), "");
-        assert_eq!(number(&mut form_1), "1");
-        assert_eq!(number(&mut form_2), "25");
-        assert_eq!(number(&mut form_3), "1");
-        assert_eq!(number(&mut form_4), "12");
+        assert_eq!(form_0.number_str(), "");
+        assert_eq!(form_1.number_str(), "1");
+        assert_eq!(form_2.number_str(), "25");
+        assert_eq!(form_3.number_str(), "1");
+        assert_eq!(form_4.number_str(), "12");
 
-        let mut form_5 = Source::new("123+456");
+        let mut form_5 = Parser::new("123+456");
 
         // i32型に変換
-        assert_eq!(number(&mut form_5).parse::<i32>().unwrap(), 123);
+        assert_eq!(form_5.number(), 123);
     }
 }
